bae09c41ae633edce06405ed29b4a5bc4fe26284
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-02-13 20:19:50 +1100

    Tweaking Docker image creation

--- crawler/src/test/java/com/ruchij/CrawlerAppTest.java
+++ crawler/src/test/java/com/ruchij/CrawlerAppTest.java
@@ -2,8 +2,30 @@ package com.ruchij;
 
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 class CrawlerAppTest {
     @Test
-    void appHasAGreeting() {
+    void appHasAGreeting() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+
+        SecretKey secretKey = keyGenerator.generateKey();
+        String key = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+
+        System.out.println("Secret Key: %s".formatted(key));
+
+        byte[] bytes = new byte[16];
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(bytes);
+
+        String iv = Base64.getEncoder().encodeToString(bytes);
+
+        System.out.println("IV: %s".formatted(iv));
     }
 }
