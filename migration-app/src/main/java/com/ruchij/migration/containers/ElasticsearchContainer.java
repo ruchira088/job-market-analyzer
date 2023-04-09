c22605feec9ddfe03213fafa045e0a61c4d82b08
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-04-09 15:04:42 +1000

    Upgrading Elastic Docker containers

--- migration-app/src/main/java/com/ruchij/migration/containers/ElasticsearchContainer.java
+++ migration-app/src/main/java/com/ruchij/migration/containers/ElasticsearchContainer.java
@@ -8,7 +8,7 @@ public class ElasticsearchContainer extends org.testcontainers.elasticsearch.Ela
 	private static final String PASSWORD = "my-password";
 
 	public ElasticsearchContainer() {
-		super("docker.elastic.co/elasticsearch/elasticsearch:8.6.2");
+		super("docker.elastic.co/elasticsearch/elasticsearch:8.7.0");
 		addEnv("xpack.security.http.ssl.enabled", "false");
 		addEnv("ELASTIC_PASSWORD", PASSWORD);
 	}
