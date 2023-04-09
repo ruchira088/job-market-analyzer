e767300cacfda6e51ac9d5570c434d4d4f1f9f0a
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-04-09 11:26:17 +1000

    Optimizing LinkedIn credentials verification

--- crawler/src/main/java/com/ruchij/crawler/service/crawler/selenium/site/LinkedIn.java
+++ crawler/src/main/java/com/ruchij/crawler/service/crawler/selenium/site/LinkedIn.java
@@ -4,6 +4,9 @@ import com.ruchij.crawler.service.crawler.selenium.driver.AwaitableWebDriver;
 import com.ruchij.crawler.service.crawler.selenium.site.pages.HomePage;
 import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 public class LinkedIn {
 	private static final String WEB_URL = "https://www.linkedin.com/login";
 	private final AwaitableWebDriver awaitableWebDriver;
@@ -29,7 +32,41 @@ public class LinkedIn {
 		WebElement submitButton = this.awaitableWebDriver.findElementByCss("button[aria-label='Sign in']");
 		submitButton.click();
 
-		this.awaitableWebDriver.findElementByCss(".global-nav__me");
+		CompletableFuture<Boolean> isSuccess =
+			CompletableFuture.supplyAsync(() -> {
+				this.awaitableWebDriver.findElementByCss(".global-nav__me");
+				return true;
+			});
+
+		CompletableFuture<Boolean> errorMessageNotVisible = CompletableFuture.supplyAsync(() -> {
+			boolean isValid = true;
+
+			while (isValid && !isSuccess.isDone()) {
+				WebElement passwordError = this.awaitableWebDriver.findElementByCss("#error-for-password");
+				isValid = passwordError.getText().isEmpty();
+
+				if (isValid) {
+					try {
+						Thread.sleep(250);
+					} catch (InterruptedException ignored) {
+					}
+				}
+			}
+
+			return isValid;
+		});
+
+		Boolean success;
+
+		try {
+			success = (Boolean) CompletableFuture.anyOf(isSuccess, errorMessageNotVisible).get(10, TimeUnit.SECONDS);
+		} catch (Exception exception) {
+			throw new RuntimeException("Something went wrong");
+		}
+
+		if (!success) {
+			throw new RuntimeException("LinkedIn login error visible");
+		}
 
 		return new HomePage(this.awaitableWebDriver);
 	}
