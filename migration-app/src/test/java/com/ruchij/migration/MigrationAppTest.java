0586b36b6226d953c6c2a5d1883ecb362d5254e8
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-04-10 23:28:35 +1000

    Adding test for Elasticsearch migration

--- migration-app/src/test/java/com/ruchij/migration/MigrationAppTest.java
+++ migration-app/src/test/java/com/ruchij/migration/MigrationAppTest.java
@@ -1,6 +1,8 @@
 package com.ruchij.migration;
 
 import com.ruchij.migration.config.DatabaseConfiguration;
+import com.ruchij.migration.config.ElasticsearchConfiguration;
+import com.ruchij.migration.containers.ElasticsearchContainer;
 import com.ruchij.migration.containers.PostgresContainer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,15 @@ class MigrationAppTest {
 			DatabaseConfiguration databaseConfiguration = postgresContainer.databaseConfiguration();
 
 			Assertions.assertDoesNotThrow(() -> MigrationApp.runDatabaseMigration(databaseConfiguration));
+		}
+	}
+
+	@Test
+	void runElasticsearchMigration() {
+		try (ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer()) {
+			ElasticsearchConfiguration elasticsearchConfiguration = elasticsearchContainer.elasticsearchConfiguration();
+
+			Assertions.assertDoesNotThrow(() -> MigrationApp.runElasticsearchMigration(elasticsearchConfiguration));
 		}
 	}
 
