4f565f676220949ded2616ac6979d31455fc2f4f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-05-20 22:18:45 +1000

    Adding tests for RedisKeyValueStore

---
+++ api/src/test/java/com/ruchij/api/kv/RedisKeyValueStoreTest.java
@@ -0,0 +1,61 @@
+package com.ruchij.api.kv;
+
+import com.ruchij.api.config.RedisConfiguration;
+import com.ruchij.api.containers.RedisContainer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.*;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RedisKeyValueStoreTest {
+	private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+
+	@Test
+	@Timeout(value = 10, unit = TimeUnit.SECONDS)
+	void shouldInsertAndRetrieveKeyValuePair() throws Exception {
+		run(redisKeyValueStore ->
+			redisKeyValueStore.put("A", "APPLE", Optional.empty())
+				.thenCompose(__ -> redisKeyValueStore.get("A"))
+				.thenAccept(maybeValue -> assertEquals(Optional.of("APPLE"), maybeValue))
+		);
+	}
+
+	@Test
+	@Timeout(value = 10, unit = TimeUnit.SECONDS)
+	void shouldExpireKeyAfterTtl() throws Exception {
+		run(redisKeyValueStore ->
+			redisKeyValueStore.put("B", "BAT", Optional.of(Duration.ofSeconds(1)))
+				.thenCompose(__ -> redisKeyValueStore.get("B"))
+				.thenAccept(maybeValue -> assertEquals(Optional.of("BAT"), maybeValue))
+				.thenCompose(__ -> sleep(Duration.ofSeconds(2)))
+				.thenCompose(__ -> redisKeyValueStore.get("B"))
+				.thenAccept(maybeValue -> assertEquals(Optional.empty(), maybeValue))
+		);
+	}
+
+	private CompletableFuture<Void> sleep(Duration duration) {
+		CompletableFuture<Void> completableFuture = new CompletableFuture<>();
+		scheduledExecutorService.schedule(
+			() -> completableFuture.complete(null),
+			duration.toMillis(),
+			TimeUnit.MILLISECONDS
+		);
+
+		return completableFuture;
+	}
+
+	private void run(Function<RedisKeyValueStore, CompletableFuture<?>> action) throws Exception {
+		try (RedisContainer redisContainer = new RedisContainer()) {
+			RedisConfiguration redisConfiguration = redisContainer.redisConfiguration();
+			RedisKeyValueStore redisKeyValueStore = new RedisKeyValueStore(redisConfiguration.uri());
+
+			action.apply(redisKeyValueStore).get();
+		}
+	}
+
+}
